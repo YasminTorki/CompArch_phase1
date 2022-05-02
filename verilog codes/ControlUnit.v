fc3fe4145ac0457ec8178b804c846c9d800d2728
YasminTorki <89246022+YasminTorki@users.noreply.github.com>
2022-05-03 02:01:47 +0430

    Create ControlUnit.v

---
+++ verilog codes/ControlUnit.v
@@ -0,0 +1,123 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:12:01 05/03/2022 
+// Design Name: 
+// Module Name:    ControlUnit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ControlUnit( input[3:0] opcode,
+      output reg[2:0] alu_op,
+      output reg jump,beq,bne,mem_read,mem_write,alu_src,reg_dst,mem_to_reg,reg_write  
+    );
+always @(*)
+begin
+ case(opcode) 
+ 4'b0001:  // LW
+   begin
+    reg_dst = 1'b0;
+    alu_src = 1'b1;
+    mem_to_reg = 1'b1;
+    reg_write = 1'b1;
+    mem_read = 1'b1;
+    mem_write = 1'b0;
+    beq = 1'b0;
+    bne = 1'b0;
+    alu_op = 3'b001;
+    jump = 1'b0;   
+   end
+ 4'b0010:  // SW
+   begin
+    reg_dst = 1'b0;
+    alu_src = 1'b1;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b0;
+    mem_read = 1'b0;
+    mem_write = 1'b1;
+    beq = 1'b0;
+    bne = 1'b0;
+    alu_op = 3'b001;
+    jump = 1'b0;   
+   end
+ 4'b0000:  // data_processing
+   begin
+    reg_dst = 1'b1;
+    alu_src = 1'b0;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b1;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    beq = 1'b0;
+    bne = 1'b0;
+    alu_op = 3'b100;
+    jump = 1'b0;   
+   end
+ 
+ 4'b0011:  // BEQ
+   begin
+    reg_dst = 1'b0;
+    alu_src = 1'b0;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b0;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    beq = 1'b1;
+    bne = 1'b0;
+    alu_op = 3'b010;
+    jump = 1'b0;   
+   end
+ 4'b0100:  // BNE
+   begin
+    reg_dst = 1'b0;
+    alu_src = 1'b0;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b0;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    beq = 1'b0;
+    bne = 1'b1;
+    alu_op = 3'b010;
+    jump = 1'b0;   
+   end
+ 4'b0111:  // Jump
+   begin
+    reg_dst = 1'b0;
+    alu_src = 1'b0;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b0;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    beq = 1'b0;
+    bne = 1'b0;
+    alu_op = 3'b000;
+    jump = 1'b1;   
+   end   
+ default: begin
+    reg_dst = 1'b1;
+    alu_src = 1'b0;
+    mem_to_reg = 1'b0;
+    reg_write = 1'b1;
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+    beq = 1'b0;
+    bne = 1'b0;
+    alu_op = 3'b000;
+    jump = 1'b0; 
+   end
+ endcase
+ end
+
+
+endmodule

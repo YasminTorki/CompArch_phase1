f23973172871f5e25e33fb2e22d2df9a6c94aea6
YasminTorki <89246022+YasminTorki@users.noreply.github.com>
2022-06-16 23:31:03 +0430

    Add files via upload

---
+++ verilog codes/Main.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:04:32 06/16/2022 
+// Design Name: 
+// Module Name:    Main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Main(
+    );
+
+
+endmodule

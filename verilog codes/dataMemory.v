2b456b9a7ef0cf235a9407ef8eedcd3da31e1806
YasminTorki <89246022+YasminTorki@users.noreply.github.com>
2022-06-16 14:22:25 +0430

    Add files via upload

---
+++ verilog codes/dataMemory.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:16:44 06/14/2022 
+// Design Name: 
+// Module Name:    dataMemory 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module dataMemory(
+   input                         clk,  
+      input     [15:0]               mem_access_addr,  
+      input     [15:0]               mem_write_data,  
+      input mem_read, mem_write_en, 
+      // read port  
+      output     [15:0]               mem_read_data  
+    );
+   integer i;  
+      reg [15:0] ram [255:0];  
+      wire [7 : 0] ram_addr = mem_access_addr[8 : 1];  
+      initial begin  
+           for(i=0; i<256;i=i+1)  
+                ram[i] <= 16'd0;  
+      end   
+      always @(posedge clk) begin  
+           if (mem_write_en)  
+                ram[ram_addr] <= mem_write_data;  
+      end  
+      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;
+endmodule
